a7330e96f43df61df509493e905f279a3b1d95fd
thiago lopes de mello <timabuntu@gmail.com>
2022-05-19 10:37:23 -0300

    Airline AppContext used in Landing Page

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,3 +1,7 @@
+import { useContext } from "react";
+import { AirlineAppContext } from "../../context/airline-context";
+
 export function LandingPage() {
-  return <h1>LandingPage</h1>;
+  const {} = useContext(AirlineAppContext);
+  return <h1>Landing Page</h1>;
 }
