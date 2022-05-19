5ad8d8cd1166ed427f600338eac6006e81f44778
thiago lopes de mello <timabuntu@gmail.com>
2022-05-19 11:58:20 -0300

    feat: created LandingPage

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,3 +1,13 @@
+import { useContext } from "react";
+import { AirlineAppContext } from "../../context/airline-context";
+import { TileList } from "../Tiles/TileList";
+
 export const LandingPage: React.FC = () => {
-  return <h1>Landing Page</h1>;
+  const { AirlineData } = useContext(AirlineAppContext);
+
+  return (
+    <>
+      <TileList airlineData={AirlineData} />
+    </>
+  );
 };
