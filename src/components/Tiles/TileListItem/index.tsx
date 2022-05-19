e871946cfa0a97b6f808e3b1748b78ab2e321064
thiago lopes de mello <timabuntu@gmail.com>
2022-05-19 11:51:44 -0300

    feat: created TileListItem

--- src/components/Tiles/TileListItem/index.tsx
+++ src/components/Tiles/TileListItem/index.tsx
@@ -1,7 +1,29 @@
-const TileListItem: React.FC = () => {
-  return (
-    <h1>TileListItem</h1>
-  );
+import { useState } from "react";
+import { AirlineData } from "../../../api/airline";
+
+interface TileProps {
+  data: AirlineData;
 }
 
-export default TileListItem;
+export const Tile: React.FC<TileProps> = ({ data }: TileProps) => {
+  const [hover, setHover] = useState(false);
+  const handleMouseHover = () => {
+    setHover(true);
+  };
+  const handleMouseOut = () => {
+    setHover(false);
+  };
+
+  return (
+    <div
+      onMouseOver={handleMouseHover}
+      onMouseOut={handleMouseOut}
+      className="bg-green-200"
+    >
+      <img src={data.logUrl} alt={data.name} />
+      <div className="">{data.name}</div>
+      <div className="">{data.phone}</div>
+      {hover && <div className="">{data.alliance}</div>}
+    </div>
+  );
+};
