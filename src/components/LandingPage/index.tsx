162dcd650bb4df21437fd903eb523effeb035089
thiago lopes de mello <timabuntu@gmail.com>
2022-05-19 12:20:25 -0300

    feat: added in landing-page components Actions and HeaderComp

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { AirlineAppContext } from "../../context/airline-context";
+import { Actions } from "../Actions";
 import { TileList } from "../Tiles/TileList";
-
+import { Header as HeaderComp } from "../Header";
 export const LandingPage: React.FC = () => {
   const { AirlineData } = useContext(AirlineAppContext);
 
   return (
     <>
+      <HeaderComp />
+      <Actions />
       <TileList airlineData={AirlineData} />
     </>
   );
